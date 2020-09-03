47b66934d80f60ef806dee496813b9eaca635ed1
Genaro <genarocoronel917@gmail.com>
2020-09-03 18:08:04 -0500

    Agregando buscador a tabla

--- src/views/Listado.js
+++ src/views/Listado.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Table, Button } from 'reactstrap'
+import { Table, Button, Input } from 'reactstrap'
 import axios from 'axios'
 import PaginationComponent from "react-reactstrap-pagination";
+import Autocomplete from "../components/Autocomplete";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Listado extends React.Component {
-  state = { results: [], activePage: 1, books: [] }
+  state = { results: [], activePage: 1, books: [], searc: '' }
 
   componentDidMount() {
     console.log('entra en component did mount')
@@ -22,12 +23,25 @@ class Listado extends React.Component {
     this.setState({activePage: pageNumber});
   }
 
+  change(e){
+    let query = e.target.value;
+    let filtrados = this.state.results.filter(result =>  result.Title.toLowerCase() === query );
+    console.log("===============");
+    console.log("Query: " + query)
+    console.log("Filtrados: " + filtrados);
+    this.setState({ results :  filtrados});
+
+  }
+
+  requestTimer = null;
+
   render() {
     return (
       <div>
+       <Input type="search" name="search" id="search" placeholder="Introduce tu busqueda" onChange={this.change.bind(this)} style={{ 'width': '500px'}}/>
         {this.state.results.length > 0 ? (
           <div>
-          <Table striped style={{ width: '700px', textAlign: 'center' }}>
+          <Table striped style={{ width: '700px', textAlign: 'center' }} bordered hover size="sm" responsive>
             <thead>
               <tr>
                 <th>ID</th>
